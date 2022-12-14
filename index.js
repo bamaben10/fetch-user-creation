d8e471605c11c39620e4299e99fd8490d36ff46f
Benjamin Salmon <bensalmon8@gmail.com>
2022-12-14 17:20:11 -0600

    js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("connected");
+var objPeople = [
+  {
+    firstName: "ben",
+    lastName: "sal",
+    password: "codify",
+    email: "sam@codify.com",
+    password: "Bama",
+  },
+];
+
+function getInfo() {
+  let firstName = document.getElementById("first").value;
+  let lastName = document.getElementById("last").value;
+  let email = document.getElementById("Email").value;
+  let password = document.getElementById("password");
+
+  for (let i = 0; i < objPeople.length; i++) {
+    if (
+      (firstName =
+        objPeople[i].firstName &&
+        lastName == objPeople[i].lastName &&
+        password == objPeople[i].password &&
+        email == objPeople[i].email)
+    ) {
+      return;
+    }
+  }
+  console.log("You have successfully created a user login");
+}
